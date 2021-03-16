ce744720f645d68a0af3f235d3df049731bb0b69
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-03-16 19:45:26 +0530

    Update bms_constants.py

--- bms_constants.py
+++ bms_constants.py
@@ -2,7 +2,7 @@ bms_thresholds = {'temperature': {'min': 0, 'max': 45}, 'soc': {'min': 20, 'max'
                   'charging_rate': {'min': 0, 'max': 0.8}}
 
 bms_system_languages = {'en': 'BMS System configured with English',
-                        'de':'BMS-System mit Deutsch konfiguriert'}
+                        'de':'BMS-System auf Englisch konfiguriert'}
 
 bms_system_temperature_unit = {'C': 'Celcius',
                         'F':'Fahrenheit'}
@@ -13,6 +13,23 @@ bms_system_temperature_unit_notification = {'en': 'BMS Temperature measurement u
 bms_input_param_status = {'en':['UNKNOWN PARAMETER','INVALID PARAMETER VALUE'],
                           'de':['UNBEKANNTER PARAMETER','UNGÜLTIGER PARAMETERWERT']}
 
-bms_input_param_range_status = {'en':['OVERLIMIT', 'UNDERLIMIT', 'All Parameters are OK','BMS Operating in Unsafe Condition:'],
+bms_console_output_messages = {'en':['OVERLIMIT', 'UNDERLIMIT', 'All Parameters are OK','BMS Operating in Unsafe Condition:'],
                                 'de':['ÜBER DEM LIMIT','UNTERBEGRENZUNG', 'Alle Parameter sind in Ordnung','BMS arbeitet in unsicherem Zustand:']}
+
+bms_hazard_warning_message = {'en': '!!! WARNING !!!','de': '!!! WARNUNG !!!'}
+
+bms_controller_flags = {'low':1,'high':0}
+
+bms_controller_output_messages ={
+                                'en': {'temperature':['Turn ON BMS Cooler', 'Turn ON BMS Heater'],
+                                        'soc':['CUT OFF BMS Charging','SWITCH BMS TO POWER SAVING MODE'],
+                                        'charging_rate':['Decrease Charging Current','Increase Charging Current']},
+                                 'de': {'temperature': ['Schalten Sie den BMS-Kühler ein', 'Schalten Sie die BMS-Heizung ein'],
+                                        'soc': ['BMS-Aufladung abschalten', 'Schalten Sie BMS in den Energiesparmodus'],
+                                        'charging_rate': ['Ladestrom verringern', 'Ladestrom erhöhen']}
+                                 }
+
+
+
+
 
