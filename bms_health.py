71f140921dbee691639493ae71dfb3873ab4c687
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-03-16 19:46:23 +0530

    Update bms_health.py

--- bms_health.py
+++ bms_health.py
@@ -1,31 +1,45 @@
 from bms_constants import bms_thresholds
-from bms_constants import bms_input_param_range_status
+from bms_constants import bms_console_output_messages
 import bms_system_configurator
+from bms_constants import bms_controller_flags
+from bms_constants import bms_hazard_warning_message
+from bms_constants import bms_controller_output_messages
 
 
 def collect_anomalies(anomaly, bms_parameter, bms_parameter_value, bms_parameter_range):
-    bms_parameter_value = BMS_temperature_haldler(bms_parameter,bms_parameter_value)
+    bms_parameter_value = BMS_temperature_haldler(bms_parameter, bms_parameter_value)
     if bms_parameter_value < bms_parameter_range['min']:
-        anomaly.append([bms_parameter, bms_input_param_range_status[bms_system_configurator.system_language][1]])
+        anomaly.append([bms_parameter, bms_console_output_messages[bms_system_configurator.system_language][1], 'low'])
     elif bms_parameter_value > bms_parameter_range['max']:
-        anomaly.append([bms_parameter, bms_input_param_range_status[bms_system_configurator.system_language][0]])
+        anomaly.append([bms_parameter, bms_console_output_messages[bms_system_configurator.system_language][0], 'high'])
 
 
 def bms_health_is_ok(status_report):
     anomalies = []
     for bms_parameter in status_report:
         collect_anomalies(anomalies, bms_parameter, status_report[bms_parameter], bms_thresholds[bms_parameter])
-    bms_breach_param(anomalies)
+    BMS_console_output(anomalies)
+    BMS_controller_output(anomalies)
     return anomalies
 
 
-def bms_breach_param(anomalies):
+def BMS_console_output(anomalies):
     if len(anomalies) == 0:
-        print(bms_input_param_range_status[bms_system_configurator.system_language][2])
+        print(bms_console_output_messages[bms_system_configurator.system_language][2])
     else:
+        print('------------------------- {} -------------------------'.format(
+            bms_hazard_warning_message[bms_system_configurator.system_language]))
         for parameter in anomalies:
-            print(bms_input_param_range_status[bms_system_configurator.system_language][3] + '{} -> {}'.format(
+            print(bms_console_output_messages[bms_system_configurator.system_language][3] + '{} -> {}'.format(
                 parameter[0], parameter[1]))
+        print('-------------------------------------------------------')
+
+
+def BMS_controller_output(anomalies):
+    if len(anomalies) != 0:
+        for parameter in anomalies:
+            print(bms_controller_output_messages[bms_system_configurator.system_language][parameter[0]][
+                      bms_controller_flags[parameter[2]]])
 
 
 def BMS_temperature_haldler(bms_parameter, bms_parameter_value):
